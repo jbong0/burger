e7e83860f8d214c4c9a3b3b433d79fff7031ae45
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-06-08 15:12:05 -0700

    Added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,26 @@
 var mysql = require("mysql")
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    database: 'burgers_db',
-    password: ""
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        database: 'burgers_db',
+        password: ""
 })
+}
 
-connection.connect(function(err){
-   if (err) throw err
-})
-
-
-module.exports = connection 
+// Make the connection to MySQL
+connection.connect(function(err) {
+    if (err) {
+      console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+      return;
+    }
+    console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+  });
+  
+  // Export connection for ORM use
+  module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -11,6 +11,7 @@ var orm = {
         })
     },
     updateOne:() => {
+     
 
     }
 }
